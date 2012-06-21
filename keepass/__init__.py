40f0fdd0e71d03865c6228ebf68ed90735d12901
Ryan Zenker <ryan@nodal.org>
2012-06-21 10:42:02 -0600

    helper function to pull passwords easily from other code.

--- keepass/__init__.py
+++ keepass/__init__.py
@@ -3,3 +3,19 @@
 KeePass module
 '''
 
+import os.path
+
+def get_entry(dbfilename, title, keyfilename=None, passphrase=None):
+
+    dbname = os.path.basename(dbfilename).split(".")[0]
+    if keyfilename is None:
+        keyfilename = os.path.abspath(os.path.dirname(dbfilename) + "/secure/" + dbname + ".key")
+
+    infile = file(keyfilename)
+    filekey = infile.read().strip().decode('hex')
+    infile.close()
+
+    db = Database(filename, filekey=filekey, passphrase=passphrase)
+    entry = db.get(title)
+
+    return entry
